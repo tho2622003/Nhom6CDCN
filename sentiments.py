a956830f40449a4baad09850811fc413c6e1163a
Nguyễn Hoàng Thọ <100546882+tho2622003@users.noreply.github.com>
2024-03-18 18:30:18 +0700

    Add files via upload
    updated sentiments.py

--- sentiments.py
+++ sentiments.py
@@ -1,25 +1,13 @@
-## Imports
-
 import pandas as pd
 import csv
 import nltk
 import os.path as checkcsv
 
-## Downloads
-
-def sepposnegcom(comment_file):
-
-    ## Reading Dataset
+def analyze(comment_file):
 
     dataset = pd.read_csv(comment_file, encoding_errors = 'ignore')
     dataset = dataset.iloc[:, 0:]
 
-    ## Getting Full Comments to csv file
-
-    # full_com = dataset
-    # full_comments = full_com.to_csv("Full Comments.csv")
-
-    ## Sentiment analysis of comments using vadar sentiment analyser
 
     from nltk.sentiment.vader import SentimentIntensityAnalyzer
     analyser = SentimentIntensityAnalyzer()
@@ -33,12 +21,11 @@ def sepposnegcom(comment_file):
 
     dataset['vader_sentiment'] = dataset['Comment'].apply(lambda x : vader_sentiment_result(x))
 
-    ## Separating Positive and Negative Comments
 
     for (sentiment), group in dataset.groupby(['vader_sentiment']):
          group.to_csv(f'{sentiment}.csv', index=False)
     
-    if checkcsv.exists('1.csv') == False:                             # If 1.csv file does not exist, it creates one empty 1.csv file.
+    if checkcsv.exists('1.csv') == False:
         with open('1.csv', 'w', encoding='UTF8', newline='') as f1:
             writer1 = csv.writer(f1)
             header1 = ['Empty', 'Empty', 'Empty']
@@ -46,7 +33,7 @@ def sepposnegcom(comment_file):
             writer1.writerow(header1)
             writer1.writerow(row1)
 
-    if checkcsv.exists('0.csv') == False:                             # If 1.csv file does not exist, it creates one empty 1.csv file.
+    if checkcsv.exists('0.csv') == False:
         with open('0.csv', 'w',encoding='UTF8', newline='') as f0:
             writer0 = csv.writer(f0)
             header0 = ['Empty', 'Empty', 'Empty']
@@ -60,13 +47,12 @@ def sepposnegcom(comment_file):
     positive_comments = pos.to_csv("Positive Comments.csv", index=False)
     negative_comments = neg.to_csv("Negative Comments.csv",index=False)
 
-    video_positive_comments = str(len(pos.axes[0])) + ' Comments'  #Finding total rows in positive comments
-    video_negative_comments = str(len(neg.axes[0])) + ' Comments'  #Finding total rows in negative comments
+    video_positive_comments = str(len(pos.axes[0])) + ' Comments'
+    video_negative_comments = str(len(neg.axes[0])) + ' Comments' 
     
     if (pd.read_csv('1.csv', nrows=0).columns.tolist())[0] == 'Empty':
         video_positive_comments = '0 Comments'
     if (pd.read_csv('0.csv', nrows=0).columns.tolist())[0] == 'Empty':
         video_negative_comments = '0 Comments'
 
-    ## return function
     return positive_comments, negative_comments, video_positive_comments, video_negative_comments
