25c3abd96a2931956f9add2743ef0877a852cec4
Lại Đức Thắng <laiducthang2003@gmail.com>
2024-03-18 23:08:26 +0700

    Update sentiments.py

--- sentiments.py
+++ sentiments.py
@@ -1,58 +1,43 @@
+import numpy as np
 import pandas as pd
-import csv
-import nltk
-import os.path as checkcsv
+from nltk.sentiment.vader import SentimentIntensityAnalyzer
+import matplotlib.pyplot as plt
 
-def analyze(comment_file):
+sia = SentimentIntensityAnalyzer()
+df = pd.read_csv('FLAT.csv', encoding_errors = 'ignore')
+df['scores'] = df['comment'].apply(lambda body: sia.polarity_scores(str(body)))
+df.head()
+df['compound'] = df['scores'].apply(lambda score_dict:score_dict['compound'])
+df.head()
+df['pos'] = df['scores'].apply(lambda pos_dict:pos_dict['pos'])
+df.head()
+df['neg'] = df['scores'].apply(lambda neg_dict:neg_dict['neg'])
+df.head()
+df['type']=''
+df.loc[df.compound>0,'type']='POS'
+df.loc[df.compound==0,'type']='NEUTRAL'
+df.loc[df.compound<0,'type']='NEG'
+df.head()
+len=df.shape
+(rows,cols)=len
+pos=0
+neg=0
+neutral=0
+for i in range(0,rows):
+    if df.loc[i][5]=="POS":
+        pos=pos+1
+    if df.loc[i][5]=="NEG":
+        neg=neg+1
+    if df.loc[i][5]=="NEUTRAL":
+        neutral=neutral+1
 
-    dataset = pd.read_csv(comment_file, encoding_errors = 'ignore')
-    dataset = dataset.iloc[:, 0:]
+print("Positive :"+str(pos) + "  Negative :" + str(neg) + "   Neutral :"+ str(neutral))
 
+# histogram sentiments
+plt.hist(df['compound'])
+plt.title("Comment Sentiment Analysis")
+plt.show()
 
-    from nltk.sentiment.vader import SentimentIntensityAnalyzer
-    analyser = SentimentIntensityAnalyzer()
-
-    def vader_sentiment_result(sent):
-        scores = analyser.polarity_scores(sent)
-
-        if scores["neg"] > scores["pos"]:
-            return 0
-        return 1
-
-    dataset['vader_sentiment'] = dataset['Comment'].apply(lambda x : vader_sentiment_result(x))
-
-
-    for (sentiment), group in dataset.groupby(['vader_sentiment']):
-         group.to_csv(f'{sentiment}.csv', index=False)
-    
-    if checkcsv.exists('1.csv') == False:
-        with open('1.csv', 'w', encoding='UTF8', newline='') as f1:
-            writer1 = csv.writer(f1)
-            header1 = ['Empty', 'Empty', 'Empty']
-            row1 = ['No Positive Comments', 'No Positive Comments', 'No Positive Comments']
-            writer1.writerow(header1)
-            writer1.writerow(row1)
-
-    if checkcsv.exists('0.csv') == False:
-        with open('0.csv', 'w',encoding='UTF8', newline='') as f0:
-            writer0 = csv.writer(f0)
-            header0 = ['Empty', 'Empty', 'Empty']
-            row0 = ['No Negative Comments', 'No Negative Comments', 'No Negative Comments']
-            writer0.writerow(header0)
-            writer0.writerow(row0)
-    
-    pos = (pd.read_csv("1.csv", engine = 'python')).iloc[:, :-1]
-    neg = (pd.read_csv("0.csv", engine = 'python')).iloc[:, :-1]
-
-    positive_comments = pos.to_csv("Positive Comments.csv", index=False)
-    negative_comments = neg.to_csv("Negative Comments.csv",index=False)
-
-    video_positive_comments = str(len(pos.axes[0])) + ' Comments'
-    video_negative_comments = str(len(neg.axes[0])) + ' Comments' 
-    
-    if (pd.read_csv('1.csv', nrows=0).columns.tolist())[0] == 'Empty':
-        video_positive_comments = '0 Comments'
-    if (pd.read_csv('0.csv', nrows=0).columns.tolist())[0] == 'Empty':
-        video_negative_comments = '0 Comments'
-
-    return positive_comments, negative_comments, video_positive_comments, video_negative_comments
+print("Average Positive: ", df['pos'].mean(), "\n")
+print("Average Negative: ", df['neg'].mean(), "\n")
+print("Average Compound: ", df['compound'].mean(), "\n")
